aebf3f2c69dfdd37a3f544c63e69d1b70a1c47f2
Alessandro Lima <78627928+alexZ7000@users.noreply.github.com>
2023-11-21 19:28:29 -0300

    Atualização de branch dev (#6)
    * tornando o projeto plugável e desplugável
    
    * remoção de arquivos que deveriam ser ignorados pelo git
    
    * Update .gitignore
    
    * Função de calcular a primeira coluna de forma binária concluida
    
    * Todas as tabelas mostram V e F de maneira correta e limpeza das listas antes do uso
    
    * Mudança de nome de classe e redimensionalização do layout dos botões para que fique melhor em monitores com escala de 125%
    
    * Mudança de nome de classe e redimensionalização do layout dos botões para que fique melhor em monitores com escala de 125%
    
    * detecção de operadores lógicos
    
    * correção da pilha dos parenteses, agora eu fecho um parenteses para cada um que foi aberto
    
    * cálculo de negação de uma proposição
    
    * Criação de outra classe em Operations, refatoração de código
    
    * Correção do bug dos parentesis
    
    * Melhoria dos imports
    
    * Conjunção de duas proposições está funcionando
    
    * Discrete Math é capaz de calcular operações de matemática discreta!
    
    * Tratamento de erros caso o Discrete Math não seje capaz de executar uma operação

--- DiscreteMath/Operations.java
+++ DiscreteMath/Operations.java
@@ -16,11 +16,13 @@ public class Operations {
     private final ArrayList<Integer> binariesColumnTwo = new ArrayList<>();
     private final ArrayList<Integer> binariesColumnThree = new ArrayList<>();
     private final ArrayList<Integer> binariesColumnFour = new ArrayList<>();
+    private final ArrayList<Integer> resultOperationBinaries = new ArrayList<>();
     private final Calculation calculation = new Calculation(
             binariesColumnOne,
             binariesColumnTwo,
             binariesColumnThree,
             binariesColumnFour,
+            resultOperationBinaries,
             vAndF
     );
     private final MainScreen instanceMainScreen;
@@ -42,38 +44,34 @@ public class Operations {
         }
         getAllBinariesColumns(characters, isNegation);
         calculation.detectLogicalOperators(entireOperationCharacters, logicalOperators);
+        calculation.convertBinariesToTrueOrFalse();
         TitleConstructor(letrasEncontradas, vAndF, entireOperationCharacters, isSinglePropositon);
     }
 
     private void getAllBinariesColumns(final String characters, boolean isNegation) {
         if (this.letrasEncontradas.length() == 1){
             if (characters.contains("¬")) isNegation = true;
-            calculation.clearAllBinariesColumns();
             calculation.getBinariesColumnOne(2, isNegation);
             isSinglePropositon = true;
         }
         else if (this.letrasEncontradas.length() == 2){
             //00
-            calculation.clearAllBinariesColumns();
             calculation.getBinariesColumnOne(4, false);
             calculation.getBinariesColumnTwo(4);
         }
         else if (this.letrasEncontradas.length() == 3) {
             //000
-            calculation.clearAllBinariesColumns();
             calculation.getBinariesColumnOne(8, false);
             calculation.getBinariesColumnTwo(8);
             calculation.getBinariesColumnThree(8);
         }
         else if (this.letrasEncontradas.length() == 4) {
             //0000
-            calculation.clearAllBinariesColumns();
             calculation.getBinariesColumnOne(16, false);
             calculation.getBinariesColumnTwo(16);
             calculation.getBinariesColumnThree(16);
             calculation.getBinariesColumnFour();
         }
-        calculation.convertBinariesToTrueOrFalse();
     }
 
     private void TitleConstructor(final StringBuilder letras, final StringBuilder vAndF, final String entireOperationCharacters, final boolean isSingleProposition){
@@ -86,8 +84,8 @@ public class Operations {
         if (!(isSingleProposition)) title.append("<th>").append(entireOperationCharacters).append("</th>");
         title.append("</tr>");
         title.append(vAndF);
-        // TODO: Melhorar a lógica de adicionar V e F na tabela
         TableConstructor(title);
+        calculation.clearAllBinariesColumns();
     }
 
     private void TableConstructor(final StringBuilder title){
@@ -118,6 +116,7 @@ public class Operations {
     }
 
     public void resultOperation(final String characters) {
+        calculation.clearAllBinariesColumns();
         final String strCharacters = (characters.substring(1, characters.length() - 1)).replaceAll(",", "");
         final String entireOperationCharacters = (characters.substring(1, characters.length() - 1).replaceAll(",", " "));
         calculateNumberOfPrepositions(strCharacters, entireOperationCharacters);
@@ -131,6 +130,7 @@ class Calculation {
     private final ArrayList<Integer> binariesColumnTwo;
     private final ArrayList<Integer> binariesColumnThree;
     private final ArrayList<Integer> binariesColumnFour;
+    private final ArrayList<Integer> resultOperationBinaries;
     private final StringBuilder vAndF;
 
     public Calculation(
@@ -138,12 +138,14 @@ class Calculation {
             final ArrayList<Integer> binariesColumnTwo,
             final ArrayList<Integer> binariesColumnThree,
             final ArrayList<Integer> binariesColumnFour,
+            final ArrayList<Integer> resultOperationBinaries,
             final StringBuilder vAndF
     ) {
         this.binariesColumnOne = binariesColumnOne;
         this.binariesColumnTwo = binariesColumnTwo;
         this.binariesColumnThree = binariesColumnThree;
         this.binariesColumnFour = binariesColumnFour;
+        this.resultOperationBinaries = resultOperationBinaries;
         this.vAndF = vAndF;
     }
 
@@ -164,6 +166,10 @@ class Calculation {
                 if (binariesColumnFour.get(i) == 1) vAndF.append("<td>V</td>");
                 else vAndF.append("<td>F</td>");
             }
+            if (!resultOperationBinaries.isEmpty()){
+                if (resultOperationBinaries.get(i) > 0) vAndF.append("<td>V</td>");
+                else vAndF.append("<td>F</td>");
+            }
             vAndF.append("</tr>");
         }
     }
@@ -174,6 +180,70 @@ class Calculation {
                 if (i >= quantityOfBinaries / 2) binariesColumnOne.add(1);
                 else binariesColumnOne.add(0);
             }
+        }
+        else {
+            for (int i = 0; i < quantityOfBinaries; ++i) {
+                if (i >= quantityOfBinaries / 2) binariesColumnOne.add(0);
+                else binariesColumnOne.add(1);
+            }
+        }
+        System.out.println("1:" + binariesColumnOne);
+    }
+
+    public void getBinariesColumnTwo(final int quantityOfBinaries) {
+        for (int i = 1; i <= quantityOfBinaries / 2; i++) {
+            binariesColumnTwo.add(1);
+            binariesColumnTwo.add(0);
+        }
+        System.out.println("2:" + binariesColumnTwo);
+    }
+
+    public void getBinariesColumnThree(final int quantityOfBinaries) {
+        for (int i = 0; i < quantityOfBinaries / 4; i++) {
+            binariesColumnThree.add(1);
+            binariesColumnThree.add(1);
+            binariesColumnThree.add(0);
+            binariesColumnThree.add(0);
+        }
+        System.out.println("3:" + binariesColumnThree);
+    }
+
+    public void getBinariesColumnFour() {
+        for (int i = 0; i < 8; i++) {
+            if (i >= 8 / 2) binariesColumnFour.add(1);
+            else binariesColumnFour.add(0);
+        }
+        System.out.println("4:" + binariesColumnFour);
+    }
+
+    public void clearAllBinariesColumns() {
+        binariesColumnOne.clear();
+        binariesColumnTwo.clear();
+        binariesColumnThree.clear();
+        binariesColumnFour.clear();
+        resultOperationBinaries.clear();
+    }
+
+    public void detectLogicalOperators(final String entireOperationCharacters, final ArrayList<String> logicalOperators) {
+        for (int i = 0; i < entireOperationCharacters.length(); i++) {
+            final char c = entireOperationCharacters.charAt(i);
+            if (logicalOperators.contains(String.valueOf(c))) {
+                if (c == '(') parenthesisMethod(entireOperationCharacters);
+                else if (c == '∧') conjunction(entireOperationCharacters, i);
+                else if (c == '∨') disjunction(entireOperationCharacters);
+                else if (c == '⊕') exclusiveDisjunction(entireOperationCharacters);
+                else if (c == '→') conditional(entireOperationCharacters);
+                else if (c == '↔') biconditional(entireOperationCharacters);
+                else if (c == '⇒') materialConditional(entireOperationCharacters);
+                else if (c == '⇔') materialBiconditional(entireOperationCharacters);
+                else if (c == '≡') equivalence(entireOperationCharacters);
+            }
+        }
+    }
+
+    private void conjunction(final String entireOperationCharacters, final int counter) {
+        for (int i = 0;  i < binariesColumnOne.size(); i++){
+            resultOperationBinaries.add(binariesColumnOne.get(i) * binariesColumnTwo.get(i));
         }
         else {
             for (int i = 0; i < quantityOfBinaries; ++i) {
@@ -240,6 +310,44 @@ class Calculation {
 
     private void disjunction(final String entireOperationCharacters) {
         System.out.println("Disjunção");
+    }
+
+    private void exclusiveDisjunction(final String entireOperationCharacters) {
+        System.out.println("Disjunção Exclusiva");
+    }
+
+    private void conditional(final String entireOperationCharacters) {
+        System.out.println("Condicional");
+    }
+
+    private void biconditional(final String entireOperationCharacters) {
+        System.out.println("Bicondicional");
+    }
+
+    private void materialConditional(final String entireOperationCharacters) {
+        System.out.println("Condicional Material");
+    }
+
+    private void materialBiconditional(final String entireOperationCharacters) {
+        System.out.println("Bicondicional Material");
+    }
+
+    private void equivalence(final String entireOperationCharacters) {
+        System.out.println("Equivalência");
+    }
+
+    private void parenthesisMethod(final String entireOperationCharacters) {
+        System.out.println("Parênteses Aberto");
+    }
+
+    private void negation(final String entireOperationCharacters) {
+        System.out.println("Negação");
+    }
+
+    private void disjunction(final String entireOperationCharacters) {
+        for (int i = 0;  i < binariesColumnOne.size(); i++){
+            resultOperationBinaries.add(binariesColumnOne.get(i) + binariesColumnTwo.get(i));
+        }
     }
 
     private void exclusiveDisjunction(final String entireOperationCharacters) {
